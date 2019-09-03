3caf85ef53a1efc21be2ad3176508ab529bee0f9
j8takagi <j8takagi@nifty.com>
2019-09-03 17:52:33 +0900

    不要なテストコードを削除

--- test_heapsort.c
+++ test_heapsort.c
@@ -20,19 +20,11 @@ void printarray(int data[], int cnt)
 int main(void) {
     int cnt = 7;
     int testdata[] = {60, 10, 20, 45, 5, 15, 30};
-    int testdata2[cnt];
     int heapdata[cnt];
     int sortdata[cnt];
 
     printf("Test data: ");
     printarray(testdata, cnt);
-
-    for(int i = 0; i < cnt; i++) {
-        testdata2[i] = testdata[i];
-    }
-    downHeap(testdata2, cnt-1);
-    printf("Test data2: ");
-    printarray(testdata2, cnt);
 
     makeHeap(testdata, heapdata, cnt);
     printf("Heap data: ");
